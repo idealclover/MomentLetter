eb5eb76a45adab3c99f337ef6751fae0736bf566
clover <shadowspacex@163.com>
2019-05-09 00:35:09 +0800

    :sparkles: add bugly

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
-//import './pages/HomePage.dart';
+import 'package:flutter_bugly/flutter_bugly.dart';
 import './pages/SendCrossPage.dart';
 
-void main() => runApp(MyApp());
+void main() => FlutterBugly.postCatchedException(() {
+      runApp(MyApp());
+    });
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterBugly.init(
+        androidAppId: "4afa1af849",
+        iOSAppId: "your iOS app id",
+        enableNotification: true,
+        autoCheckUpgrade: true);
+
     return MaterialApp(
       title: 'MomentMachine',
       theme: ThemeData(

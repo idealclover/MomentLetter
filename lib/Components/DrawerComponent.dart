20deac22b5a6d76d8712a67f236d8b0e2db86c99
clover <shadowspacex@163.com>
2019-05-09 19:04:42 +0800

    :sparkles: add  custom infomation

--- lib/Components/DrawerComponent.dart
+++ lib/Components/DrawerComponent.dart
@@ -9,14 +9,12 @@ import 'dart:convert';
 import '../pages/SendCrossPage.dart';
 import '../pages/SettingPage.dart';
 
-
 class DrawerComponent extends StatefulWidget {
   @override
   _DrawerComponentState createState() => _DrawerComponentState();
 }
 
 class _DrawerComponentState extends State<DrawerComponent> {
-
   Future<Map> _get(List<String> strList) async {
     Map rst = new Map();
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -26,7 +24,7 @@ class _DrawerComponentState extends State<DrawerComponent> {
     return rst;
   }
 
-  Future<String> _getAvatar() async{
+  Future<String> _getAvatar() async {
     return _getEmail().then((String email) {
       var content = new Utf8Encoder().convert(email);
       var digest = md5.convert(content);
@@ -34,19 +32,23 @@ class _DrawerComponentState extends State<DrawerComponent> {
     });
   }
 
-  Future<String> _getWebSite() async{
+  Future<String> _getWebSite() async {
     List<String> strList = ['url'];
     return _get(strList).then((Map rstList) {
-      if (rstList[strList[0]] == null) return "idealclover.top";
-      else return rstList[strList[0]];
+      if (rstList[strList[0]] == null)
+        return "idealclover.top";
+      else
+        return rstList[strList[0]];
     });
   }
 
-  Future<String> _getEmail() async{
+  Future<String> _getEmail() async {
     List<String> strList = ['email'];
     return _get(strList).then((Map rstList) {
-      if (rstList[strList[0]] == null) return "idealclover@163.com";
-      else return rstList[strList[0]];
+      if (rstList[strList[0]] == null)
+        return "idealclover@163.com";
+      else
+        return rstList[strList[0]];
     });
   }
 
@@ -56,26 +58,29 @@ class _DrawerComponentState extends State<DrawerComponent> {
       UserAccountsDrawerHeader(
         currentAccountPicture: new FutureBuilder(
             future: _getAvatar(),
-            builder:(context, AsyncSnapshot<String> avatar){
-              if(avatar.hasData && avatar.data != null){
+            builder: (context, AsyncSnapshot<String> avatar) {
+              if (avatar.hasData && avatar.data != null) {
                 return CircleAvatar(
                   backgroundImage: NetworkImage(avatar.data),
                 );
-              } else return Text("");
+              } else
+                return Text("");
             }),
         accountName: new FutureBuilder(
             future: _getWebSite(),
-            builder:(context, AsyncSnapshot<String> website){
-            if(website.hasData && website.data != null){
-              return Text(website.data);
-            } else return Text("");
-        }),
+            builder: (context, AsyncSnapshot<String> website) {
+              if (website.hasData && website.data != null) {
+                return Text(website.data);
+              } else
+                return Text("");
+            }),
         accountEmail: new FutureBuilder(
             future: _getEmail(),
-            builder:(context, AsyncSnapshot<String> email){
-              if(email.hasData && email.data != null){
+            builder: (context, AsyncSnapshot<String> email) {
+              if (email.hasData && email.data != null) {
                 return Text(email.data);
-              } else return Text("");
+              } else
+                return Text("");
             }),
       ),
       ListTile(
@@ -94,33 +99,30 @@ class _DrawerComponentState extends State<DrawerComponent> {
             FlutterBugly.getUpgradeInfo().then((UpgradeInfo info) {
               if (info != null && info.id != null) {
                 showDialog(
-                  barrierDismissible: false,
-                  context: context,
-                  builder: (BuildContext context){
-                    return AlertDialog(
-                        title: new Text(info.title),
-                        content: new Text(info.newFeature),
-                        actions: <Widget>[
-                          new FlatButton(
-                            child: new Text("去升级"),
-                            onPressed: () async{
-                              Navigator.of(context).pop();
-                              if(await canLaunch(info.apkUrl)){
-                                await launch(info.apkUrl);
-                              } else {
-                                Scaffold.of(context)
-                                    .showSnackBar(SnackBar(content: Text("打开链接失败")));
-                              }
-                            },
-                          ),
-                          new FlatButton(
-                            child: new Text("下次再说"),
-                            onPressed: () => Navigator.of(context).pop()
-                          )
-                        ]
-                    );
-                  }
-                );
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                          title: new Text(info.title),
+                          content: new Text(info.newFeature),
+                          actions: <Widget>[
+                            new FlatButton(
+                              child: new Text("去升级"),
+                              onPressed: () async {
+                                Navigator.of(context).pop();
+                                if (await canLaunch(info.apkUrl)) {
+                                  await launch(info.apkUrl);
+                                } else {
+                                  Scaffold.of(context).showSnackBar(
+                                      SnackBar(content: Text("打开链接失败")));
+                                }
+                              },
+                            ),
+                            new FlatButton(
+                                child: new Text("下次再说"),
+                                onPressed: () => Navigator.of(context).pop())
+                          ]);
+                    });
               }
             });
           }),
@@ -130,7 +132,8 @@ class _DrawerComponentState extends State<DrawerComponent> {
           trailing: Icon(Icons.free_breakfast),
           onTap: () async {
             Navigator.of(context).pop();
-            if(await canLaunch("https://qr.alipay.com/tsx01807zuhjvzgmt5xmtb0")){
+            if (await canLaunch(
+                "https://qr.alipay.com/tsx01807zuhjvzgmt5xmtb0")) {
               await launch("https://qr.alipay.com/tsx01807zuhjvzgmt5xmtb0");
             } else {
               Scaffold.of(context)
@@ -143,7 +146,7 @@ class _DrawerComponentState extends State<DrawerComponent> {
           trailing: Icon(Icons.computer),
           onTap: () async {
             Navigator.of(context).pop();
-            if(await canLaunch("https://idealclover.top")){
+            if (await canLaunch("https://idealclover.top")) {
               await launch("https://idealclover.top");
             } else {
               Scaffold.of(context)
